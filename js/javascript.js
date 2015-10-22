bef41e62bc6c0fc3adcc44def906d7d0c09a5a7c
darkdanito <darkdanito@hotmail.com>
2015-10-22 14:21:27 +0800

    Version: 3.4
    - Updated the buttons for the different colors

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-function myFunction()
+function myFunctionPostIT()
 {
 	var output = document.getElementById('canvas');
 	
@@ -8,6 +8,46 @@ function myFunction()
 		ele.setAttribute("id","timedrpact");
 		ele.setAttribute("class","drag");
 		ele.setAttribute("style","width:20%");
+//		ele.style.backgroundColor = "green";
+		ele.innerHTML=document.getElementById('textbox1').value;
+		output.appendChild(ele);
+}
+
+function myFunctionPostITRed()
+{
+	var output = document.getElementById('canvas');
+	
+	var ele = document.createElement("div");
+		ele.setAttribute("id","timedrpact");
+		ele.setAttribute("class","drag");
+		ele.setAttribute("style","width:20%");
+		ele.style.backgroundColor = "red";
+		ele.innerHTML=document.getElementById('textbox1').value;
+		output.appendChild(ele);
+}
+
+function myFunctionPostITBlue()
+{
+	var output = document.getElementById('canvas');
+	
+	var ele = document.createElement("div");
+		ele.setAttribute("id","timedrpact");
+		ele.setAttribute("class","drag");
+		ele.setAttribute("style","width:20%");
+		ele.style.backgroundColor = "blue";
+		ele.innerHTML=document.getElementById('textbox1').value;
+		output.appendChild(ele);
+}
+
+function myFunctionPostITGreen()
+{
+	var output = document.getElementById('canvas');
+	
+	var ele = document.createElement("div");
+		ele.setAttribute("id","timedrpact");
+		ele.setAttribute("class","drag");
+		ele.setAttribute("style","width:20%");
+		ele.style.backgroundColor = "green";
 		ele.innerHTML=document.getElementById('textbox1').value;
 		output.appendChild(ele);
 }
