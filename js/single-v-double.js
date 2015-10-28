223b75a2283165c458bb8bedeb51dfce0cf93004
Biri Biri - YY <darkdanito@hotmail.com>
2015-10-28 23:26:02 +0800

    Version: 6.2
    - JS Code cleanup

--- js/single-v-double.js
+++ js/single-v-double.js
@@ -1,3 +1,66 @@
+	function myFunctionPostIT()
+	{
+		var output = document.getElementById('canvas');
+	
+		var uniqid = Date.now();
+	
+		var ele = document.createElement("div");
+			ele.setAttribute("id",uniqid);
+			ele.setAttribute("class","drag");
+			ele.setAttribute("style","width:20%");
+	//		ele.style.backgroundColor = "green";
+			ele.style.border = "thick solid black";
+			ele.innerHTML=document.getElementById('textbox1').value;
+			output.appendChild(ele);
+	}
+	
+	function myFunctionPostITRed()
+	{
+		var output = document.getElementById('canvas');
+		
+		var uniqid = Date.now();
+		
+		var ele = document.createElement("div");
+			ele.setAttribute("id",uniqid);
+			ele.setAttribute("class","drag");
+			ele.setAttribute("style","width:20%");
+	//		ele.style.backgroundColor = "red";
+	//		ele.style.borderColor = "red";
+			ele.style.border = "thick solid red";
+			ele.innerHTML=document.getElementById('textbox1').value;
+			output.appendChild(ele);
+	}
+	
+	function myFunctionPostITBlue()
+	{
+		var output = document.getElementById('canvas');
+		
+		var uniqid = Date.now();
+		
+		var ele = document.createElement("div");
+			ele.setAttribute("id",uniqid);
+			ele.setAttribute("class","drag");
+			ele.setAttribute("style","width:20%");
+			ele.style.border = "thick solid blue";
+			ele.innerHTML=document.getElementById('textbox1').value;
+			output.appendChild(ele);
+	}
+	
+	function myFunctionPostITGreen()
+	{
+		var output = document.getElementById('canvas');
+	
+		var uniqid = Date.now();
+		
+		var ele = document.createElement("div");
+			ele.setAttribute("id",uniqid);
+			ele.setAttribute("class","drag");
+			ele.setAttribute("style","width:20%");
+			ele.style.border = "thick solid green";
+			ele.innerHTML=document.getElementById('textbox1').value;
+			output.appendChild(ele);
+	}
+	
 	function $(id)
 	{
 		return document.getElementById(id);
